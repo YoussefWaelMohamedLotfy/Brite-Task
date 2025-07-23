558e9bfc105c68322c8fa732c72c024cd0336b0c
YoussefWaelMohamedLotfy <youssefwael8@gmail.com>
2025-07-23 13:08:29 +0300

    Added Containers for Garnet, Keycloak and PostgreSQL in Aspire

--- src/Aspire/Brite-Task.AppHost/AppHost.cs
+++ src/Aspire/Brite-Task.AppHost/AppHost.cs
@@ -1,5 +1,29 @@
 IDistributedApplicationBuilder builder = DistributedApplication.CreateBuilder(args);
 
-builder.AddProject<Projects.EM_API>("api");
+var garnet = builder.AddGarnet("garnet")
+    .WithImage("microsoft/garnet-alpine")
+    .WithImageTag("latest")
+    .WithLifetime(ContainerLifetime.Persistent);
+
+var postgres = builder.AddPostgres("postgres")
+    .WithImageTag("alpine")
+    .WithDataVolume()
+    .WithPgAdmin(x => x.WithImageTag("latest")
+        .WithHostPort(5050)
+        .WithLifetime(ContainerLifetime.Persistent)
+    )
+    .WithLifetime(ContainerLifetime.Persistent);
+
+var postgresdb = postgres.AddDatabase("Employee-Management-Db");
+
+var keycloak = builder.AddKeycloak("keycloak", 8081)
+    .WithImageTag("latest")
+    .WithDataVolume()
+    .WithLifetime(ContainerLifetime.Persistent);
+
+builder.AddProject<Projects.EM_API>("api")
+    .WithReference(keycloak)
+    .WithReference(garnet)
+    .WithReference(postgresdb);
 
 await builder.Build().RunAsync();
