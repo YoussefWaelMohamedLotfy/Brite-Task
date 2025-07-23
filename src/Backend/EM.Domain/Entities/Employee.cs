9d4ab4bcc87749022e90d079712db7e4c8ec41f9
YoussefWaelMohamedLotfy <youssefwael8@gmail.com>
2025-07-23 13:22:00 +0300

    Added Employee Entity

---
+++ src/Backend/EM.Domain/Entities/Employee.cs
@@ -0,0 +1,24 @@
+﻿using EM.Domain.Common;
+
+namespace EM.Domain.Entities;
+
+public sealed class Employee : Entity<Guid>, IAuditableEntity
+{
+    public string Name { get; set; } = default!;
+    
+    public string Email { get; set; } = default!;
+    
+    public string Phone { get; set; } = default!;
+    
+    public DateTimeOffset DateOfJoining { get; set; }
+    
+    public bool IsActive { get; set; }
+
+    public Guid CreatedBy { get; init; }
+    
+    public DateTimeOffset CreatedAt { get; init; }
+    
+    public Guid UpdatedBy { get; init; }
+    
+    public DateTimeOffset UpdatedAt { get; init; }
+}
