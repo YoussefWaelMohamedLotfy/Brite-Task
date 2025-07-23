099656669d9aea17f0a861b8d2dd98b89eb5775f
YoussefWaelMohamedLotfy <youssefwael8@gmail.com>
2025-07-23 13:16:57 +0300

    Added Base Classes for Entities

---
+++ src/Backend/EM.Domain/Common/Entity.cs
@@ -0,0 +1,6 @@
+﻿namespace EM.Domain.Common;
+
+public abstract class Entity<T> where T : notnull
+{
+    public T ID { get; init; } = default!;
+}
---
+++ src/Backend/EM.Domain/Common/IAuditableEntity.cs
@@ -0,0 +1,12 @@
+﻿namespace EM.Domain.Common;
+
+public interface IAuditableEntity
+{
+    public Guid CreatedBy { get; init; }
+    
+    public DateTimeOffset CreatedAt { get; init; }
+    
+    public Guid UpdatedBy { get; init; }
+    
+    public DateTimeOffset UpdatedAt { get; init; }
+}
