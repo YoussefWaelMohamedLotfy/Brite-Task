367d96e65625d8178379f626fef01ce03883f189
YoussefWaelMohamedLotfy <youssefwael8@gmail.com>
2025-07-23 14:00:08 +0300

    Created Department Entity

---
+++ src/Backend/EM.Domain/Entities/Department.cs
@@ -0,0 +1,18 @@
+﻿using EM.Domain.Common;
+
+namespace EM.Domain.Entities;
+
+public sealed class Department : Entity<int>, IAuditableEntity
+{
+    public string Name { get; set; } = default!;
+
+    public string? Description { get; set; }
+
+    public Guid CreatedBy { get; init; }
+
+    public DateTimeOffset CreatedAt { get; init; }
+
+    public Guid UpdatedBy { get; init; }
+
+    public DateTimeOffset UpdatedAt { get; init; }
+}
