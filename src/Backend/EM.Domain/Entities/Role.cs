c9e0f31329281f36153a6a9d45d33a0695510a66
YoussefWaelMohamedLotfy <youssefwael8@gmail.com>
2025-07-23 14:17:48 +0300

    Added Role Entity

---
+++ src/Backend/EM.Domain/Entities/Role.cs
@@ -0,0 +1,18 @@
+﻿using EM.Domain.Common;
+
+namespace EM.Domain.Entities;
+
+public sealed class Role : Entity<int>, IAuditableEntity
+{
+    public string Name { get; set; } = default!;
+
+    public List<string> Permissions { get; set; } = default!;
+
+    public Guid CreatedBy { get; init; }
+
+    public DateTimeOffset CreatedAt { get; init; }
+
+    public Guid UpdatedBy { get; init; }
+
+    public DateTimeOffset UpdatedAt { get; init; }
+}
