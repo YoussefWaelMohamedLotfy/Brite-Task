88757b86444c54dd76cb82c7a72d36d861e6a225
YoussefWaelMohamedLotfy <youssefwael8@gmail.com>
2026-04-19 19:41:40 +0200

    Migrate to Tunit testing

--- tests/EM.Domain.UnitTests/EmployeeTests.cs
+++ tests/EM.Domain.UnitTests/EmployeeTests.cs
@@ -4,8 +4,8 @@ namespace EM.Domain.UnitTests;
 
 public sealed class EmployeeTests
 {
-    [Fact]
-    public void ToggleEmployeeActivationStatus_EmployeeIsInactive_ShouldToggleStatusToTrue()
+    [Test]
+    public async Task ToggleEmployeeActivationStatus_EmployeeIsInactive_ShouldToggleStatusToTrue()
     {
         // Arrange
         Employee employee = new() { ID = Guid.CreateVersion7(), IsActive = false };
@@ -14,11 +14,11 @@ public sealed class EmployeeTests
         employee.ToggleActivation();
 
         // Assert
-        Assert.True(employee.IsActive);
+        await Assert.That(employee.IsActive).IsTrue();
     }
 
-    [Fact]
-    public void ToggleEmployeeActivationStatus_EmployeeIsActive_ShouldToggleStatusToFalse()
+    [Test]
+    public async Task ToggleEmployeeActivationStatus_EmployeeIsActive_ShouldToggleStatusToFalse()
     {
         // Arrange
         Employee employee = new() { ID = Guid.CreateVersion7(), IsActive = true };
@@ -27,6 +27,6 @@ public sealed class EmployeeTests
         employee.ToggleActivation();
 
         // Assert
-        Assert.False(employee.IsActive);
+        await Assert.That(employee.IsActive).IsFalse();
     }
 }
